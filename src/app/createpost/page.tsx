c24921eed36e9a4a3a6a7cf0490197893c5b1a95
Code Manch <136045573+codemanch@users.noreply.github.com>
2023-08-18 12:43:51 +0000

    WYSIWYG not working now

--- src/app/createpost/page.tsx
+++ src/app/createpost/page.tsx
@@ -1,41 +1,39 @@
 "use client";
 import Footer from "@/UI/footer";
 import NAV from "@/UI/nav";
-import { getConsole } from "./getConsole";
-import { useEffect, useState } from "react";
+
+import { useEffect, useState, useRef } from "react";
 
 const Post = () => {
   const [imageDataURL, setImageDataUrl] = useState("");
-  const handleFileChange = (event: any) => {
-    const selectedFile = event.target.files[0];
-    if (selectedFile) {
-      const reader = new FileReader();
-      reader.onload = (e) => {
-        setImageDataUrl(e.target?.result as string);
-      };
-      reader.readAsDataURL(selectedFile);
-    }
+  const [content, setContent] = useState("");
+  const [edit, setEdit] = useState(true);
+
+  const editToggle = () => {
+    setEdit(!edit);
   };
 
   useEffect(() => {
-    console.log(imageDataURL);
-  }, [imageDataURL]);
+    // console.log(imageDataURL);
+    console.log(content);
+  }, [imageDataURL, content]);
+
   return (
     <div className="flex flex-col">
       <NAV />
       <div className="w-full my-5  p-2 flex">
         <div className="max-w-[900px] mx-auto w-full mt-14 flex flex-col gap-2  items-center justify-center">
-          <form action={getConsole} className="flex gap-2 flex-col">
-            <div className="p-2 border border-gray-800 rounded-md">
+          <div className="flex w-full gap-2 flex-col">
+            <div className="p-1 border-b border-gray-700">
               <input
                 type="text"
-                placeholder="title"
+                placeholder="Title"
                 className="outline-none ring-0"
                 name="title"
                 id="title"
               />
             </div>
-            <div className="p-2 border border-gray-800 rounded-md">
+            <div className="p-1 border-b border-gray-700">
               <input
                 type="text"
                 placeholder="Desc"
@@ -44,7 +42,7 @@ const Post = () => {
                 id="desc"
               />
             </div>
-            <div className="p-2 border border-gray-800 rounded-md">
+            {/* <div className="p-2 border border-gray-800 rounded-md">
               <input
                 type="file"
                 className="outline-none ring-0"
@@ -53,15 +51,15 @@ const Post = () => {
                 id="image"
                 onChange={handleFileChange}
               />
-            </div>
-
+            </div> */}
+            <div id="editorjs"></div>
             <button
-              className="bg-blue-700 rounded-md text-white px-4 py-2"
-              type="submit"
+              className="bg-blue-700 w-fit rounded-md text-white px-4 py-2"
+              onClick={editToggle}
             >
               Add to Cart
             </button>
-          </form>
+          </div>
         </div>
       </div>
       <Footer />
