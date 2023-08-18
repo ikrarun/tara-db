094085042149c9da892a3764e919c6327596ab52
Code Manch <136045573+codemanch@users.noreply.github.com>
2023-08-18 15:34:28 +0000

    Now Going Offline

--- src/app/createpost/page.jsx
+++ src/app/createpost/page.jsx
@@ -1,22 +1,27 @@
 "use client";
 import Footer from "@/UI/footer";
 import NAV from "@/UI/nav";
-
-import { useEffect, useState, useRef } from "react";
+import { useEffect } from "react";
 
 const Post = () => {
-  const [imageDataURL, setImageDataUrl] = useState("");
-  const [content, setContent] = useState("");
-  const [edit, setEdit] = useState(true);
-
-  const editToggle = () => {
-    setEdit(!edit);
-  };
-
+  var editor;
   useEffect(() => {
-    // console.log(imageDataURL);
-    console.log(content);
-  }, [imageDataURL, content]);
+    const init = async () => {
+      const Editor = (await import("@editorjs/editorjs")).default;
+      const Header = (await import("@editorjs/header")).default;
+      const InlineCode = (await import("@editorjs/code")).default;
+      editor = new Editor({
+        tools: {
+          // ... your tools
+          header: Header,
+          inlineCode: InlineCode,
+        },
+      });
+    };
+    init();
+
+    return editor?.destroy();
+  }, []);
 
   return (
     <div className="flex flex-col">
@@ -24,7 +29,7 @@ const Post = () => {
       <div className="w-full my-5  p-2 flex">
         <div className="max-w-[900px] mx-auto w-full mt-14 flex flex-col gap-2  items-center justify-center">
           <div className="flex w-full gap-2 flex-col">
-            <div className="p-1 border-b border-gray-700">
+            <div className="p-2 border rounded-md border-gray-700/40">
               <input
                 type="text"
                 placeholder="Title"
@@ -33,7 +38,7 @@ const Post = () => {
                 id="title"
               />
             </div>
-            <div className="p-1 border-b border-gray-700">
+            <div className="p-2 border rounded-md border-gray-700/40">
               <input
                 type="text"
                 placeholder="Desc"
@@ -42,21 +47,12 @@ const Post = () => {
                 id="desc"
               />
             </div>
-            {/* <div className="p-2 border border-gray-800 rounded-md">
-              <input
-                type="file"
-                className="outline-none ring-0"
-                name="image"
-                accept="image/*"
-                id="image"
-                onChange={handleFileChange}
-              />
-            </div> */}
-            <div id="editorjs"></div>
-            <button
-              className="bg-blue-700 w-fit rounded-md text-white px-4 py-2"
-              onClick={editToggle}
-            >
+            <h1 className="border-b border-dashed my-4 border-gray-700/40" />
+            <div
+              id="editorjs"
+              className="p-2 border border-gray-700/40  rounded-md "
+            />
+            <button className="bg-blue-700 w-fit rounded-md text-white px-4 py-2">
               Add to Cart
             </button>
           </div>
