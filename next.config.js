8df2d60e21403cf68d5bac7b7a64b3b10167b89d
codemanch <wowdeveloper@protonmail.com>
2023-08-24 23:20:21 +0530

    image source in config js

--- next.config.js
+++ next.config.js
@@ -24,6 +24,12 @@ const nextConfig = {
         port: "",
         pathname: "/**",
       },
+      {
+        protocol: "https",
+        hostname: "taradb.vercel.app",
+        port: "",
+        pathname: "/**",
+      },
     ],
   },
 };
