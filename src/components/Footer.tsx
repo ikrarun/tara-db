2b9fa46233824d583f6f5fc258f7bfe6faef1620
codemanch <wowdeveloper@protonmail.com>
2023-08-24 16:10:15 +0530

    Color Changed

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,6 +1,7 @@
 import { BsInstagram, BsEnvelope, BsTwitter } from "react-icons/bs";
+import Link from "next/link";
 const Footer = () => (
-  <div className="w-full h-fit p-2 mt-2 flex  bg-gray-950 text-white sticky top-0 ">
+  <div className="w-full h-fit p-2 mt-2 flex  bg-blue-700 text-white sticky top-0 ">
     <div className="max-w-[900px] gap-4 p-4 mx-auto w-full flex flex-col items-center justify-center">
       <h1 className="font-light text-sm sm:text-base break-before-auto">
         TARA DB - Truth and Awareness Resource Archive
@@ -8,12 +9,12 @@ const Footer = () => (
       <h1 className="font-light text-xs sm:text-sm text-center break-before-auto">
         {message}
       </h1>
-      <div className="flex flex-row items-center gap-3 justify-center">
+      {/* <div className="flex flex-row items-center gap-3 justify-center">
         <h1>{<BsTwitter />}</h1>
         <h1>{<BsInstagram />}</h1>
         <h1>{<BsEnvelope />}</h1>
-      </div>
-      <h1 className="font-light text-xs">Crafted By: Kr. Arun</h1>
+      </div> */}
+      <Link href='https://github.com/codemanch' className="font-light underline underline-offset-2 text-xs">Crafted By: Kr. Arun</Link>
       <h1 className="font-light  text-xs">Copyright &#64; 2023 | CodeManch </h1>
     </div>
   </div>
--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -9,7 +9,7 @@ const font = Font({ subsets: ["latin"] });
 export const Nav = async () => {
   const user = await getServerAuthSession();
   return (
-    <div className="sticky top-0 w-full mb-5 flex-col h-fit p-2 flex z-[10000]  bg-gray-950 text-white ">
+    <div className="sticky top-0 w-full mb-5 flex-col h-fit p-2 flex z-[10000]  bg-blue-700 text-white ">
       <div className={font.className}>
         <div className="w-full flex-col flex ">
           <div className="max-w-[900px] relative grow-0 mx-auto  w-full inline-flex items-center justify-between">
