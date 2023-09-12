bc7185b5f2bf0c3c43a5bbec2bceefd1bcfd416c
ikrarun <wowdeveloper@protonmail.com>
2023-09-12 10:03:46 +0530

    Header Adjusted

--- src/components/Nav/Header.tsx
+++ src/components/Nav/Header.tsx
@@ -39,13 +39,11 @@ export default function Header() {
   }, [pathname]);
   return (
     <div className="inline-flex top-0 left-0 right-0   text-white z-[2000] bg-blue-700 w-full fixed p-3 justify-start items-center">
-      <div className="flex w-full flex-row items-center mx-auto max-w-[900px] justify-between">
-        <h1 className="capitalize w-full px-2 font-semibold text-xl">
-          {realPath ?? "Home"}
-        </h1>
-        <Link href="/" className={"capitalize px-2 font-semibold text-xl"}>
-          <RiHome6Line />
+      <div className="  text-base w-full gap-2 inline-flex items-center  mx-auto max-w-[900px]">
+        <Link href="/" className={" text-xl capitalize inline-flex items-center  font-normal "}>
+        <RiHome6Line />
         </Link>
+           {realPath ?? "Home"}
       </div>
     </div>
   );
